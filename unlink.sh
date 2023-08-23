a1667e1cc056984636dbd6fb42a8830d06c38b9e
ao0000 <ao0000dev@gmail.com>
2023-08-23 23:48:07 +0900

    Add unlink script

---
+++ unlink.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+DOTFILES_PATH=$HOME/.dotfiles
+
+# bash
+rm $HOME/.bashrc
+rm $HOME/.bash_profile
+
+# vim
+rm $HOME/.vimrc
+
+# git
+rm $HOME/.gitconfig
+
+# tmux
+rm $HOME/.tmux.conf
+
+# bash on OSX
+rm $HOME/.hushlogin
