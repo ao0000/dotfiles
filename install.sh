974f2b3897ff70f3db6740bf13dde31b40267218
ao0000 <ao0000dev@gmail.com>
2023-08-23 23:47:49 +0900

    Add disable last login message on osx's Terminal

--- install.sh
+++ install.sh
@@ -111,6 +111,9 @@ function create_symbolic(){
 
   # tmux
   ln -sf $DOTFILES_PATH/tmux/.tmux.conf $HOME/.tmux.conf
+
+  # hide last login message on OSX
+  ln -sf $DOTFILES_PATH/osx/.hushlogin $HOME/.hushlogin
 }
 
 function setup_git(){
